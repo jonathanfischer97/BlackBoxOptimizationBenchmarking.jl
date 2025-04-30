0f9a6258c99fe01e05d10c9d0c6a7cce29957a70
Jonathan Fischer PEPE <jfisch27@jhu.edu>
2025-04-30 16:05:09 -0400

    update module to include and export adapter functions

--- src/BlackBoxOptimizationBenchmarking.jl
+++ src/BlackBoxOptimizationBenchmarking.jl
@@ -1,16 +1,21 @@
 module BlackBoxOptimizationBenchmarking
 
     using Distributions, Memoize, Optimization, Optim
-    using LinearAlgebra, RecipesBase
+    using LinearAlgebra, RecipesBase, BenchmarkTools
 
     import Optim: minimum, minimizer
     import Base: show
 
     export BBOBFunction, BenchmarkSetup
     
+    # Optimization.jl adapter exports
+    export to_optimization_function, to_optimization_problem
+    export run_bbob_benchmark, compare_optimizers
+    
     include("BBOBFunction.jl")
     include("benchmark.jl")
     include("plot_benchmark.jl")
     include("plot_functions.jl")
+    include("bbo_adapter.jl")
 
 end # module
