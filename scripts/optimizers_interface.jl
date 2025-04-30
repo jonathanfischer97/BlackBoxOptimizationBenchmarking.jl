acf357fd966e0102cc3a96d1690aef291c2009f4
Jonathan Fischer PEPE <jfisch27@jhu.edu>
2025-04-30 16:05:56 -0400

    update optimizers interface to use optimization.jl

--- scripts/optimizers_interface.jl
+++ scripts/optimizers_interface.jl
@@ -1,4 +1,5 @@
 using Optim, BlackBoxOptim, NLopt, PyCall
+using Optimization, OptimizationBBO, OptimizationOptimJL, OptimizationNLopt
 
 import BlackBoxOptimizationBenchmarking: minimizer, minimum, optimize 
 import Base.string
@@ -8,25 +9,29 @@ pinit(D) = 10*rand(D).-5
 
 # Define optimize, minimum and minimizer for each optimizer
 
-## NLopt
+## NLopt via OptimizationNLopt
 
     mutable struct NLoptOptimMethod 
         s::Symbol
     end
     string(opt::NLoptOptimMethod) = string("NLopt.", opt.s)
 
-    function optimize(NLmeth::NLoptOptimMethod, f, D, run_length)  
-        opt = Opt(NLmeth.s, D)
-        min_objective!(opt, (p, g) -> f(p) ) #NLopt expect gradient
-        maxeval!(opt, run_length)
-        xtol_abs!(opt, 1e-12)
-        xtol_rel!(opt, 1e-12)
-        ftol_abs!(opt, 1e-12)
-        ftol_rel!(opt, 1e-12)
-        lower_bounds!(opt, -5.5*ones(D))
-        upper_bounds!(opt, +5.5*ones(D))
-        minf, minx, ret = NLopt.optimize(opt, pinit(D))
-        return NLmeth, minx, minf
+    function optimize(NLmeth::NLoptOptimMethod, f, D, run_length)
+        opt_f = OptimizationFunction((x, p) -> f(x), Optimization.AutoForwardDiff())
+        prob = OptimizationProblem(opt_f, pinit(D), nothing, lb=-5.5*ones(D), ub=5.5*ones(D))
+        
+        # Create NLopt optimizer through OptimizationNLopt
+        optimizer = OptimizationNLopt.NLopt.Opt(NLmeth.s, D)
+        
+        # Configure the optimizer
+        OptimizationNLopt.NLopt.maxeval!(optimizer, run_length)
+        OptimizationNLopt.NLopt.xtol_abs!(optimizer, 1e-12)
+        OptimizationNLopt.NLopt.xtol_rel!(optimizer, 1e-12)
+        OptimizationNLopt.NLopt.ftol_abs!(optimizer, 1e-12)
+        OptimizationNLopt.NLopt.ftol_rel!(optimizer, 1e-12)
+        
+        sol = solve(prob, optimizer, maxiters=run_length)
+        return NLmeth, sol.u, sol.objective
     end
     minimum(mfit::Tuple{NLoptOptimMethod, Vector{Float64}, Float64}) = mfit[3]
     minimizer(mfit::Tuple{NLoptOptimMethod, Vector{Float64}, Float64}) = mfit[2]
@@ -53,14 +58,13 @@ pinit(D) = 10*rand(D).-5
 
 ## python cma
 
-#try
-
     @pyimport cma
     
     struct PyCMA
     end
 
     function optimize(m::PyCMA, f, D, run_length)
+        # Use the Python interface directly, as we don't have an Optimization.jl wrapper for Python CMA
         es = cma.CMAEvolutionStrategy(pinit(D), 3, Dict("verb_log"=>0, "verb_disp"=>0, "maxfevals"=>run_length))
         mfit = es.optimize(f).result
         (m, mfit[1], mfit[2])
@@ -78,6 +82,7 @@ pinit(D) = 10*rand(D).-5
         method::String
     end
 
+    # We keep the direct Python interface for scipy, as we don't have an Optimization.jl wrapper
     optimize(m::PyMinimize, f, D, run_length) = (m, scipy_opt.minimize(
         f, pinit(D), method=m.method,
         options = Dict(
@@ -90,30 +95,34 @@ pinit(D) = 10*rand(D).-5
     
     string(m::PyMinimize) = string("Py.", m.method)
     
-#end
-    
-## Optim
+## Optim via OptimizationOptimJL
 
-    optimize(opt::Optim.AbstractOptimizer, f, D, run_length) =
-        Optim.optimize(f, pinit(D), opt, Optim.Options(f_calls_limit=run_length, g_tol=1e-120, iterations=run_length))
-                
-    optimize(opt::Optim.AbstractOptimizer, f, D, run_length, xinit) =
-        Optim.optimize(f, xinit, opt, Optim.Options(f_calls_limit=run_length, g_tol=1e-120, iterations=run_length))
-        
-    optimize(opt::Optim.SAMIN, f, D, run_length) =
-        Optim.optimize(
-            f, fill(-5.5, D), fill(5.5, D), pinit(D), opt, 
-            Optim.Options(f_calls_limit=run_length, g_tol=1e-120, iterations=run_length, show_trace=false)
-        )
+    function optimize(opt::Optim.AbstractOptimizer, f, D, run_length)
+        opt_f = OptimizationFunction((x, p) -> f(x), Optimization.AutoForwardDiff())
+        prob = OptimizationProblem(opt_f, pinit(D), nothing)
+        sol = solve(prob, opt, maxiters=run_length)
+        return sol
+    end
+            
+    function optimize(opt::Optim.AbstractOptimizer, f, D, run_length, xinit)
+        opt_f = OptimizationFunction((x, p) -> f(x), Optimization.AutoForwardDiff())
+        prob = OptimizationProblem(opt_f, xinit, nothing)
+        sol = solve(prob, opt, maxiters=run_length)
+        return sol
+    end
+    
+    function optimize(opt::Optim.SAMIN, f, D, run_length)
+        opt_f = OptimizationFunction((x, p) -> f(x), Optimization.AutoForwardDiff())
+        prob = OptimizationProblem(opt_f, pinit(D), nothing, lb=fill(-5.5, D), ub=fill(5.5, D))
+        sol = solve(prob, opt, maxiters=run_length)
+        return sol
+    end
 
     string(opt::Optim.AbstractOptimizer) = string(typeof(opt).name)
 
     # Optim with restart
-    try 
-        mutable struct OptimRestart{T}
-            opt::T
-        end
-    catch
+    mutable struct OptimRestart{T}
+        opt::T
     end
 
     function optimize(opt::OptimRestart, f, D, run_length) 
@@ -124,15 +133,26 @@ pinit(D) = 10*rand(D).-5
 
     string(opt::OptimRestart) = string("Restart-", string(opt.opt))
 
-## BlackBoxOptim
+## BlackBoxOptim via OptimizationBBO
 
     mutable struct BlackBoxOptimMethod 
         s::Symbol
     end
     string(opt::BlackBoxOptimMethod) = string("BBO.", opt.s)
     
-    optimize(method::BlackBoxOptimMethod, f, D, run_length) =
-        bboptimize(f; SearchRange=box(D), NumDimensions=D, Method=method.s, MaxFuncEvals=run_length, TraceMode=:silent)
+    function optimize(method::BlackBoxOptimMethod, f, D, run_length)
+        opt_f = OptimizationFunction((x, p) -> f(x))
+        prob = OptimizationProblem(opt_f, pinit(D), nothing, lb=fill(-5.5, D), ub=fill(5.5, D))
+        
+        # Map the old method symbol to the new BBO_ optimizer
+        # This maps old symbols like :adaptive_de_rand_1_bin to BBO_adaptive_de_rand_1_bin()
+        method_name = string(method.s)
+        bbo_optimizer = getproperty(OptimizationBBO, Symbol("BBO_" * method_name))()
+        
+        sol = solve(prob, bbo_optimizer, maxiters=run_length, TraceMode=:silent)
+        return sol
+    end
 
-    minimum(mfit::BlackBoxOptim.OptimizationResults) = best_fitness(mfit)
-    minimizer(mfit::BlackBoxOptim.OptimizationResults) = best_candidate(mfit)
+    # These methods work with Optimization.jl solution objects
+    minimum(sol::Optimization.SciMLBase.OptimizationSolution) = sol.objective
+    minimizer(sol::Optimization.SciMLBase.OptimizationSolution) = sol.u
